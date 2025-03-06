196e95596c3127e19b5907436c19e8b36af4333d
Futym <mfutyma2000@gmail.com>
2025-03-06 08:02:41 +0100

    configure upload and serving of the static files

--- meetups/models.py
+++ meetups/models.py
@@ -8,3 +8,4 @@ class Meetup(models.Model):
     # location = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
     description = models.TextField()
+    image = models.ImageField(upload_to="images")
