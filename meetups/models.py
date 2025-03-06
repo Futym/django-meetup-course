3bb41769bb573af7a94877b34ff2cfca42aec1ed
Futym <mfutyma2000@gmail.com>
2025-03-06 20:19:08 +0100

    add meetup location

--- meetups/models.py
+++ meetups/models.py
@@ -3,12 +3,22 @@ from django.db import models
 # Create your models here.
 
 
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"{self.name} ({self.address})"
+
+
 class Meetup(models.Model):
     title = models.CharField(max_length=100)
-    # location = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to="images")
+    location = models.ForeignKey(
+        Location, on_delete=models.CASCADE
+    )  # what will happen if a location is deleted?
 
     def __str__(self):
         return f"{self.title} - {self.slug}"
