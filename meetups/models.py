e14f4c493816d7bd76146d86df77be6ce3265bc4
Futym <mfutyma2000@gmail.com>
2025-03-06 07:11:01 +0100

    add first model, make first migration

--- meetups/models.py
+++ meetups/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Meetup(models.Model):
+    title = models.CharField(max_length=100)
+    # location = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
