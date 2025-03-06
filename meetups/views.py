368540539064a9947e6da2d609969cab08c5405a
Futym <mfutyma2000@gmail.com>
2025-03-06 07:29:02 +0100

    query data

--- meetups/views.py
+++ meetups/views.py
@@ -1,30 +1,23 @@
 from django.shortcuts import render
 
+from .models import Meetup
+
+
 # Create your views here.
 
 
 def index(request):
-    meetups = [
-        {
-            "title": "A First Meetup",
-            "location": "Happy Avenue 34",
-            "slug": "a-firs-meetup",
-        },
-        {
-            "title": "A Second Meetup",
-            "location": "Sad Avenue 35",
-            "slug": "a-second-meetup",
-        },
-    ]
-    return render(
-        request, "meetups/index.html", {"show_meetups": True, "meetups": meetups}
-    )
+    meetups = Meetup.objects.all()
+    return render(request, "meetups/index.html", {"meetups": meetups})
 
 
 def meetup_details(request, meetup_slug):
-    print(meetup_slug)
-    selected_meetup = {
-        "title": "A First Meetup",
-        "description": "This is the first meetup!",
-    }
-    return render(request, "meetups/meetup-details.html", {"meetup": selected_meetup})
+    try:
+        selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        return render(
+            request,
+            "meetups/meetup-details.html",
+            {"meetup": selected_meetup, "meetup_found": True},
+        )
+    except Exception as exc:
+        return render(request, "meetups/meetup-details.html", {"meetup_found": False})
