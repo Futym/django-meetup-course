e2e56ae3a568845c12557278e82814d1bf3931a2
Futym <mfutyma2000@gmail.com>
2025-03-09 09:32:23 +0100

    handle organizer email

--- meetups/urls.py
+++ meetups/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
     path("meetups/", views.index, name="all-meetups"),
-    path("meetups/confirmation", views.confirmation, name="confirmation"),
+    path(
+        "meetups/<slug:meetup_slug>/confirmation",
+        views.confirmation,
+        name="confirmation",
+    ),
     path("meetups/<slug:meetup_slug>/", views.meetup_details, name="meetup-detail"),
 ]
--- meetups/views.py
+++ meetups/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 from .forms import RegistrationForm
-from .models import Meetup
+from .models import Meetup, Participant
 
 
 # Create your views here.
@@ -20,9 +20,10 @@ def meetup_details(request, meetup_slug):
         else:
             registration_form = RegistrationForm(request.POST)
             if registration_form.is_valid():
-                participant = registration_form.save()
+                user_email = registration_form.cleaned_data["email"]
+                participant, _ = Participant.objects.get_or_create(email=user_email)
                 selected_meetup.participants.add(participant)
-                return redirect("confirmation")
+                return redirect("confirmation", meetup_slug=meetup_slug)
         return render(
             request,
             "meetups/meetup-details.html",
@@ -37,5 +38,10 @@ def meetup_details(request, meetup_slug):
         return render(request, "meetups/meetup-details.html", {"meetup_found": False})
 
 
-def confirmation(request):
-    return render(request, "meetups/confirmation.html")
+def confirmation(request, meetup_slug):
+    meetup = Meetup.objects.get(slug=meetup_slug)
+    return render(
+        request,
+        "meetups/confirmation.html",
+        {"organizer_email": meetup.organizer_email},
+    )
