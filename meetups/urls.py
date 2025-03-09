94f70574d233dbd1e387f0280cae7442ee481554
Futym <mfutyma2000@gmail.com>
2025-03-09 09:40:08 +0100

    optimizing urls

--- meetups/urls.py
+++ meetups/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("meetups/", views.index, name="all-meetups"),
+    path("", views.index, name="all-meetups"),
     path(
-        "meetups/<slug:meetup_slug>/confirmation",
+        "<slug:meetup_slug>/confirmation",
         views.confirmation,
         name="confirmation",
     ),
-    path("meetups/<slug:meetup_slug>/", views.meetup_details, name="meetup-detail"),
+    path("<slug:meetup_slug>/", views.meetup_details, name="meetup-detail"),
 ]
