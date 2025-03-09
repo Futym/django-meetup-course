0ad8a1aea2586a6c26733418ba57763bad2d89c4
Futym <mfutyma2000@gmail.com>
2025-03-09 09:04:27 +0100

    add registration form

--- meetups/urls.py
+++ meetups/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("meetups/", views.index, name="all-meetups"),
+    path("meetups/confirmation", views.confirmation, name="confirmation"),
     path("meetups/<slug:meetup_slug>/", views.meetup_details, name="meetup-detail"),
 ]
--- meetups/views.py
+++ meetups/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .forms import RegistrationForm
 from .models import Meetup
 
 
@@ -14,10 +15,27 @@ def index(request):
 def meetup_details(request, meetup_slug):
     try:
         selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        if request.method == "GET":
+            registration_form = RegistrationForm()
+        else:
+            registration_form = RegistrationForm(request.POST)
+            if registration_form.is_valid():
+                participant = registration_form.save()
+                selected_meetup.participants.add(participant)
+                return redirect("confirmation")
         return render(
             request,
             "meetups/meetup-details.html",
-            {"meetup": selected_meetup, "meetup_found": True},
+            {
+                "meetup": selected_meetup,
+                "meetup_found": True,
+                "registration_form": registration_form,
+            },
         )
     except Exception as exc:
+        print(exc)
         return render(request, "meetups/meetup-details.html", {"meetup_found": False})
+
+
+def confirmation(request):
+    return render(request, "meetups/confirmation.html")
